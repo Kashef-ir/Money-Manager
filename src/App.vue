7bdac308b5d35a982621be800083d3580809bf0a
kashef-ir <muhamadhashemi142@gmail.com>
2025-05-28 07:45:52 +0330

    Add TransactionList

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
 export default {
   components: {
     Header,
     Balance,
     IncomeExpenses,
+    TransactionList,
   },
 }
 </script>
@@ -17,4 +19,5 @@ export default {
     <Balance />
   </div>
   <IncomeExpenses />
+  <TransactionList />
 </template>
