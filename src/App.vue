cb514c8c54fb9792070dc1c2437ca8e8c0b436ad
kashef-ir <muhamadhashemi142@gmail.com>
2025-05-28 08:13:55 +0330

    refactor TransactionList

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
-<script>
+<script setup>
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
-}
+
+import { ref } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'Sandals', amount: -19.99 },
+  { id: 2, text: 'Books', amount: +14.99 },
+  { id: 3, text: 'AirPods', amount: -79.99 },
+  { id: 4, text: 'Tea', amount: +0.99 },
+])
 </script>
 
 <template>
@@ -21,6 +21,6 @@ export default {
     <Balance />
   </div>
   <IncomeExpenses />
-  <TransactionList />
+  <TransactionList :transactions="transactions" />
   <AddTransaction />
 </template>
