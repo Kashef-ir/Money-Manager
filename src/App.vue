dd84501d9b8bf5f71501813b179eecb376b05bc5
kashef-ir <muhamadhashemi142@gmail.com>
2025-05-28 09:13:54 +0330

    Add Form functionality

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@ import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
-
+import { useToast } from 'vue-toastification'
 import { ref, computed } from 'vue'
+
+const toast = useToast()
 
 const transactions = ref([
   { id: 1, text: 'Sandals', amount: -19.99 },
@@ -37,14 +39,27 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2)
 })
+
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateID(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  })
+  toast.success('Transaction Added')
+}
+
+const generateID = () => {
+  return Math.floor(Math.random() * 1000000)
+}
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
+    <Balance :total="+total" />
   </div>
-  <IncomeExpenses :income="income" :expenses="expenses" />
+  <IncomeExpenses :income="+income" :expenses="+expenses" />
   <TransactionList :transactions="transactions" />
-  <AddTransaction />
+  <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
 </template>
